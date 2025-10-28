0c04117392cd7af493e6bbd38412aaa02b5e0295
chaimaJr <chaima.jerbi2@gmail.com>
2025-10-28 22:17:57 +0100

    [backend] auth: user login with email instead of username

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,7 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+        def create_user(self, email, password=None, **extra_fields):
+            if not email:
+                raise ValueError('The Email field must be set')
+            email = self.normalize_email(email)
+            user = self.model(email=email, **extra_fields)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+
+        def create_superuser(self, email, password=None, **extra_fields):
+            extra_fields.setdefault('is_staff', True)
+            extra_fields.setdefault('is_superuser', True)
+
+            if extra_fields.get('is_staff') is not True:
+                raise ValueError('Superuser must have is_staff=True.')
+            if extra_fields.get('is_superuser') is not True:
+                raise ValueError('Superuser must have is_superuser=True.')
+
+            return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -12,10 +33,20 @@ class CustomUser(AbstractUser):
         ("seller", "Seller"),
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="plant_owner")
-
+    
+    email = models.EmailField(unique=True, blank=False, null=False)
+    username = None  # Remove the default username field
+    
+    # Use email as username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Email is already required by USERNAME_FIELD
+    
     profile_picture = models.ImageField(upload_to="profiles/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # Use the custom manager
+    objects = CustomUserManager()
+    
     def get_role(self):
         return self.role
